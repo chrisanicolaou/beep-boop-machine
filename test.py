d1cefe7b510405cbfaaa3a84e91f9d0d97d194a3
chrisanicolaou <chris.anicolaou@yahoo.co.uk>
2024-01-12 21:08:19 +0000

    Add test file

---
+++ test.py
@@ -0,0 +1,21 @@
+from Raspi_MotorHAT import Raspi_MotorHAT
+
+import time
+import atexit
+
+mh = Raspi_MotorHAT(addr=0x6f)
+lm = mh.getMotor(1)
+rm = mh.getMotor(2)
+
+def turn_off_motors():
+    lm.run(Raspi_MotorHAT.RELEASE)
+    rm.run(Raspi_MotorHAT.RELEASE)
+
+atexit.register(turn_off_motors)
+
+lm.setSpeed(255)
+rm.setSpeed(255)
+
+lm.run(Raspi_MotorHAT.BACKWARD)
+rm.run(Raspi_MotorHAT.BACKWARD)
+time.sleep(2)
