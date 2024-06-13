69cb263a91722a22c5da8a466ea9332bad91eddf
chrisanicolaou <christo.anicolaou@gmail.com>
2024-06-13 21:53:31 +0100

    Check state in encoder_counter to guard against wobbly encoder

--- encoder_counter.py
+++ encoder_counter.py
@@ -5,10 +5,22 @@ from gpiozero import DigitalInputDevice
 
 class EncoderCounter(object):
     def __on_pin_changed(self, _, state):
+        if self.state == state:
+            self.logger.info(f"COUNT SKIPPED DUE TO DUPLICATE STATE! Pin number: {self.pin_num}. Count before increment: {self.pulse_count}. _: {_}. State: {state}")
+            return
+        self.logger.info(f"COUNT DETECTED. Pin number: {self.pin_num}. Count before increment: {self.pulse_count}. _: {_}. State: {state}")
+        self.state = state
         self.pulse_count += 1
 
     def __init__(self, pin_num):
+        self.state = 2
         self.pulse_count = 0
+        self.last_event_time = -1
+        self.pin_num = pin_num
         self.__encoder = DigitalInputDevice(pin_num)
         self.__encoder.pin.when_changed = self.__on_pin_changed
+        file_name = os.path.basename(__file__)
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(file_name)
+        self.logger.setLevel(logging.INFO)
 
--- encoders_test.py
+++ encoders_test.py
@@ -8,19 +8,19 @@ file_name = os.path.basename(__file__)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(file_name)
 logger.setLevel(logging.INFO)
-left_encoder = EncoderCounter(5)
 right_encoder = EncoderCounter(6)
+left_encoder = EncoderCounter(5)
 motor_controller = MotorController()
 
-motor_controller.set_left(80)
-motor_controller.set_right(40)
+# motor_controller.set_left(50)
+# motor_controller.set_right(50)
 
-stop_at_time = time.time() + 1.5
+# stop_at_time = time.time() + 30
 
-while time.time() < stop_at_time:
-    logger.info(f"Left: {left_encoder.pulse_count} Right: {right_encoder.pulse_count}")
-    time.sleep(0.05)
-
-# while right_encoder.pulse_count < 40:
-#     logger.info(f"Left: {left_encoder.pulse_count} Right: {right_encoder.pulse_count}")
+# while time.time() < stop_at_time:
+#     # logger.info(f"Left: {left_encoder.pulse_count} Right: {right_encoder.pulse_count}")
 #     time.sleep(0.05)
+
+while left_encoder.pulse_count < 100:
+    # logger.info(f"Left: {left_encoder.pulse_count} Right: {right_encoder.pulse_count}")
+    time.sleep(0.05)
